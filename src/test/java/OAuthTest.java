d0c74780c69ca2a4c24e2158f8526bba622ca15b
Saumya Singh <singhsaumyait034@gmail.com>
2024-09-12 16:33:04 +0530

    AccessToken Request

--- src/test/java/OAuthTest.java
+++ src/test/java/OAuthTest.java
@@ -2,9 +2,23 @@ import static io.restassured.RestAssured.given;
 
 public class OAuthTest {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        String response = given().queryParam("access_token","")
+        String accessTokenResponse = given().
+                queryParams("code", "")
+                .queryParams("client_id", "692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com")
+                .queryParams("client_secret", "erZOWM9g3UtwNRj340YYaK_W")
+                .queryParams("grant_type", "authorization_code")
+                .queryParams("state", "verifyfjdss")
+                .queryParams("session_state", "ff4a89d1f7011eb34eef8cf02ce4353316d9744b..7eb8")
+                .queryParams("redirect_uri", "https://rahulshettyacademy.com/getCourse.php")
+                .when().log().all()
+                .post("https://www.googleapis.com/oauth2/v4/token")
+                .asString();
+
+
+        String response = given().contentType("application/json").log().all().
+                queryParam("access_token", "")
                 .when()
                 .get("https://rahulshettyacademy.com/getCourse.php")
                 .asString();
