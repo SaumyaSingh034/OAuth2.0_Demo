de22185c22f09dae4adc78a804800519af8d914e
Saumya Singh <singhsaumyait034@gmail.com>
2024-09-12 16:52:49 +0530

    OAuth Automation Completed

--- src/test/java/OAuthTest.java
+++ src/test/java/OAuthTest.java
@@ -1,11 +1,28 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.restassured.path.json.JsonPath;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 import static io.restassured.RestAssured.given;
 
 public class OAuthTest {
+    public static WebDriver driver;
 
     public static void main(String[] args) {
 
-        String accessTokenResponse = given().
-                queryParams("code", "")
+        String url ="https://rahulshettyacademy.com/getCourse.php?state=verifyfjdss&code=" +
+                "4%2FvAHBQUZU6o4WJ719NrGBzSELBFVBI9XbxvOtYpmYpeV47bFVExkaxWaF_XR14PHtTZf7ILSEeamywJKwo_BYs9M" +
+                "&scope=email+https%3A%2F%2Fwww.googleapis.com%2Fauth%" +
+                "2Fuserinfo.email+openid&authuser=0&session_state=0c32992f0d47e93d273922018ade42d1072b9d1f.." +
+                "a35c&prompt=none#";
+
+        String partialCode = url.split("code=")[1];
+        String code = partialCode.split("&scope")[0];
+        System.out.println(code);
+
+        String accessTokenResponse = given().urlEncodingEnabled(false).
+                queryParams("code", code)
                 .queryParams("client_id", "692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com")
                 .queryParams("client_secret", "erZOWM9g3UtwNRj340YYaK_W")
                 .queryParams("grant_type", "authorization_code")
@@ -16,9 +33,14 @@ public class OAuthTest {
                 .post("https://www.googleapis.com/oauth2/v4/token")
                 .asString();
 
+        JsonPath js = new JsonPath(accessTokenResponse);
+        String access_token = js.getString("access_token");
+
+        System.out.println(accessTokenResponse);
+
 
         String response = given().contentType("application/json").log().all().
-                queryParam("access_token", "")
+                queryParam("access_token", access_token)
                 .when()
                 .get("https://rahulshettyacademy.com/getCourse.php")
                 .asString();
